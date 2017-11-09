2098df609e2f0e76fdccb5abf499aae3a8d616bb
Radha <kradhaswathi@gmail.com>
2017-11-09 13:22:26 +0530

    Fixed Navigation Issue
    Fixed Navigation Issue

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import {Scene,Router} from 'react-native-router-flux';
-import Login from './src/components/Login';
-import expo from 'expo';
+import Login from '../components/Login';
 const RouterComponent =()=>{
 return(
   <Router>
-  <Scene key='login' Component={Login} title='please login'/>
+  <Scene key='login' component={Login} title='please login'/>
   </Router>
 );
-
-
 };
 
 export default RouterComponent;
